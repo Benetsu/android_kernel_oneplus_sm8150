89bd50fd555242dda0e8f00979c1fd4ff4ead98f
KAI-Miru <110590275+KAI-Miru@users.noreply.github.com>
2026-09-07 16:28:48 +0800

    sched: initialize Wave 28 boot task UX linkage

--- include/linux/init_task.h
+++ include/linux/init_task.h
@@ -223,9 +223,17 @@ extern struct cred init_cred;
  *  INIT_TASK is used to set up the first task table, touch at
  * your own risk!. Base=0, limit=0x1fffff (=2MB)
  */
+#ifdef OPLUS_FEATURE_SCHED_ASSIST
+#define INIT_OPLUS_SCHED_ASSIST(tsk)					\
+	.ux_entry = LIST_HEAD_INIT(tsk.ux_entry),
+#else
+#define INIT_OPLUS_SCHED_ASSIST(tsk)
+#endif
+
 #define INIT_TASK(tsk)	\
 {									\
 	INIT_TASK_TI(tsk)						\
+	INIT_OPLUS_SCHED_ASSIST(tsk)				\
 	.state		= 0,						\
 	.stack		= init_stack,					\
 	.usage		= ATOMIC_INIT(2),				\
